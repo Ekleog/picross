7cbbcfaf694bbf14da00c77c2ee63d25e2c44e6e
Léo Gaspard <leo@gaspard.io>
2015-12-05 22:47:49 +0100

    Add skeleton for Picross struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,28 @@
+
+enum Cell {
+    Unknown,
+    Black,
+    White,
+}
+
+struct Picross {
+    height: usize,
+    length: usize,
+
+    cells: Vec<Vec<Cell>>, // Used as cells[y][x]
+}
+
+impl Picross {
+    fn parse(string: &str) -> Picross {
+        Picross {
+            height: 0,
+            length: 0,
+            cells: vec![],
+        }
+    }
+}
+
 #[test]
 fn it_works() {
+    let _ = Picross { height: 0, length: 0, cells: vec![], };
 }
