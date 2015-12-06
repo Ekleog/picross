69c86d2575063aa284f258bc07adde0e85f812ed
Léo Gaspard <leo@gaspard.io>
2015-12-06 03:07:08 +0100

    Allow to display a picross board

--- src/lib.rs
+++ src/lib.rs
@@ -65,6 +65,61 @@ impl Picross {
 
         res
     }
+
+    fn specs_to_strings(specs: &Vec<Vec<usize>>) -> Vec<String> {
+        specs.iter()
+             .map(|v| {
+                 v.iter()
+                  .map(|x| x.to_string())
+                  .collect::<Vec<String>>()
+                  .join(" ")
+             })
+             .collect()
+    }
+
+    fn max_len_non_empty(specs: &Vec<String>) -> usize {
+        specs.iter()
+             .map(|x| x.len())
+             .max()
+             .expect("Not supporting empty picross grids!")
+    }
+
+    pub fn to_string(&self) -> String {
+        let row_spec = Picross::specs_to_strings(&self.row_spec);
+        let col_spec = Picross::specs_to_strings(&self.col_spec);
+
+        let max_rs_len = Picross::max_len_non_empty(&row_spec);
+        let max_cs_len = Picross::max_len_non_empty(&col_spec);
+
+        let line_begin = vec![" "; max_rs_len].join("");
+
+        let mut res: String = "".to_string();
+
+        for i in 0..max_cs_len {
+            res = res + &line_begin + "|";
+            for c in &col_spec {
+                res.push(match c.chars().nth(max_cs_len - i - 1) {
+                    Some(x) => x,
+                    None    => ' '
+                });
+            }
+            res.push('\n');
+        }
+
+        res = res + &vec!["-"; max_rs_len].join("") + "+" + &vec!["-"; self.length].join("") + "\n";
+
+        for i in 0..self.height {
+            res = res + &vec![" "; max_rs_len - row_spec[i].len()].join("") + &row_spec[i] + "|";
+            res = res + &self.cells[i].iter().map(|c| match c {
+                &Cell::Unknown => '?',
+                &Cell::White   => ' ',
+                &Cell::Black   => '#'
+            }).collect::<String>();
+            res.push('\n');
+        }
+
+        res
+    }
 }
 
 #[test]
@@ -97,4 +152,26 @@ fn parsing_works() {
     assert!(picross.cells[3][4] == Cell::Unknown);
     assert!(picross.row_spec[2] == vec![1, 1]);
     assert!(picross.col_spec[7] == vec![2, 2]);
+}
+
+#[test]
+fn displaying_works() {
+    let picross = Picross {
+        height: 3,
+        length: 3,
+        row_spec: vec![vec![1, 1], vec![], vec![1]],
+        col_spec: vec![vec![1, 1], vec![1], vec![2]],
+        cells: vec![vec![Cell::Unknown, Cell::White  , Cell::Black],
+                    vec![Cell::White  , Cell::White  , Cell::Black],
+                    vec![Cell::Black  , Cell::Unknown, Cell::Unknown]],
+    };
+    let res =
+        "   |1  \n".to_string() +
+        "   |   \n" +
+        "   |112\n" +
+        "---+---\n" +
+        "1 1|? #\n" +
+        "   |  #\n" +
+        "  1|#??\n";
+    assert!(picross.to_string() == res);
 }
